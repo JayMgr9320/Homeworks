59b0ae850ed3b41ccba36f715fe0e1bdc04391d3
J B <jay_rana19@yahoo.com>
2022-01-21 18:07:55 -0500

    added swapnumbers.java file

---
+++ SwapNumbers.java
@@ -0,0 +1,42 @@
+
+public class SwapNumbers {
+private int num1, num2, num3, num4; 
+	
+	public void  SwapNums (int num1, int num2, int num3, int num4) {
+		
+		this.num1 = num1; 
+		this.num2 = num2; 
+		this.num3 = num3; 
+		this.num4 = num4; 
+	}
+	public void SwapNums (int num1, int num2) {
+		this.num1 = num1; 
+		this.num2 = num2; 
+		
+	}
+	public void swap2numbersWithTempVar( int num1, int num2) {
+		int temp = 0; 
+		temp = num1; 
+		num1 = num2; 
+		num2 = temp; 
+		
+	}
+	public void swap4numbersWithTempVar( int num1, int num2, int num3, int num4) {
+		
+	}
+	public void swap3NumbersWithoutTempVar( int num1, int num2, int num3) {
+		
+	}
+	public void swap4NumbersWithoutTempVar( int num1, int num2, int num3, int num4) {
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+		
+
+}
