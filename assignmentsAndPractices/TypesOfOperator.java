70ebdb0ed9ad716c90e54e18f006593ac7c8c2ea
J B <jay_rana19@yahoo.com>
2022-01-21 19:50:20 -0500

    Added new method

--- assignmentsAndPractices/TypesOfOperator.java
+++ assignmentsAndPractices/TypesOfOperator.java
@@ -8,10 +8,17 @@ public class TypesOfOperator {
 		System.out.println(minNum);
 		
 	}
+	public void booleanOperator(int num1, int num2) {
+		boolean result; 
+		result = (num1> num2) ? true:false;
+		System.out.println(result);
+		
+		
+	}
 	public static void main(String[] args) {
 		TypesOfOperator op = new TypesOfOperator(); 
 		op.ternaryOperator(5, 7);
-	
+		op.booleanOperator(11, 17);
 	}
 
 }
